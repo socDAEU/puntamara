1d02c44dd5db0cd0788e02ffab794349991c7f3a
socDAEU <102960810+socDAEU@users.noreply.github.com>
2022-04-04 23:22:40 +0300

    Create installer_MISP.sh

---
+++ installer_MISP.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+# PLEAE DO NOT EDIT!!!
+
+apt update -y
+apt dist-upgrade -y
+	sleep 3;
+	echo "Please wait ...";
+			sudo -u puntamara bash INSTALL.sh -c -M 
+				sleep 3;
+			echo "All listening ports"
+			netstat -tulpn
+		exit 0;
