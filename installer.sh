effa54fd9f4032a337836bd96fba428eeff39f53
Your Name <you@example.com>
2022-04-04 18:58:30 +0000

    Upgrade

--- installer.sh
+++ installer.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/bash
 # PLEAE DO NOT EDIT!!!
-cd /opt/SOC_ENV/puntamara/
+cd puntamara/
 	bash puntamara.sh
-	cd 
+	cd ..
 	sleep 3;
-cd /opt/SOC_ENV/thehive/
+cd thehive/
 docker-compose up -d
-sleep 3;
-echo "Please wait ...";
 	sleep 3;
-	echo "The installation is finished!"
+	echo "Please wait ...";
+	cd ..
+	sleep 3;
+	echo "The installation is not finished..."
+			sudo -u puntamara bash INSTALL.sh -c -M 
 		exit 0;
