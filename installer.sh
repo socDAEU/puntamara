bf499dc9e43932028f04e41adb0cb0294ebe023d
Your Name <you@example.com>
2022-04-05 06:19:08 +0000

    Latest-release

--- installer.sh
+++ installer.sh
@@ -4,15 +4,19 @@
 apt update -y
 apt dist-upgrade -y
 apt install net-tools vim git docker.io docker-compose -y
+apt autoremove -y
 systemctl start docker.service
 systemctl enable docker.service
-
+systemctl restart docker.service
+## ENV
 cd puntamara/
+#	docker-compose up -d
+	cd puntamara/
 	bash puntamara.sh
 	cd ..
 	sleep 3;
 cd thehive/
-docker-compose up -d
+	docker-compose up -d
 	sleep 3;
 	echo "Please wait ...";
 		netstat -tulpn
--- puntamara/puntamara.sh
+++ puntamara/puntamara.sh
@@ -3,4 +3,4 @@ docker run -d -p 8000:8000 -p 30777:9443 --name portainer \
     --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v portainer_data:/data \
-    portainer/portainer-ce:2.9.3
+    portainer/portainer-ce:latest
